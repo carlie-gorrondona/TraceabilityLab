8c3bfe39b0653b52e9831151b668aae4cdc49a1e
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-02-24 14:53:44 -0600

    added app.use and app.get

--- server/server.js
+++ server/server.js
@@ -6,5 +6,10 @@ app.use(express.json());
 
 const port = process.env.PORT || 4546;
 
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
 app.listen(port, () => console.log(`Hephaestus override on Cauldron port ${port}`));
